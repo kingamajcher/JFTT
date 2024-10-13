8ee7334963133de998ab993d3520b203ab42e4c4
Kinga <272354@student.pwr.edu.pl>
2024-10-13 19:57:04 +0200

    comments added

--- lista1/KMP_matching.py
+++ lista1/KMP_matching.py
@@ -1,25 +1,24 @@
 from functions import *
 from Automaton import Automaton
 
+# builds a DFA for the Knuth-Morris-Pratt algorithm for given pattern and alphabet
 def build_kmp_automaton(pattern, alphabet):
-    m = len(pattern)
-    pi = compute_prefix_function(pattern)
+    
     dfa = Automaton()
-
-    # Add states
-    for idx in range(m + 1):
+    pattern_len = len(pattern)
+    pi = compute_prefix_function(pattern)
+    
+    for idx in range(pattern_len + 1):
         dfa.add_state(idx)
     
-    # Set starting state
     dfa.set_starting_state(0)
     
-    # Add accepting state
-    dfa.add_accepting_state(m)
+    dfa.add_accepting_state(pattern_len)
 
-    # Add transitions
-    for idx in range(m + 1):
+    # adds transitions for each state and each character in the alphabet
+    for idx in range(pattern_len + 1):
         for char in alphabet:
-            if idx < m and pattern[idx] == char:
+            if idx < pattern_len and pattern[idx] == char:
                 next_state = idx + 1
             else:
                 next_state = kmp_automaton_transition(pi[idx - 1], char, pattern, pi) if idx > 0 else 0
@@ -27,19 +26,21 @@ def build_kmp_automaton(pattern, alphabet):
     
     return dfa
 
-
+# uses the KMP DFA build from the pattern to find matches in the text
 def kmp_automaton_match(pattern, text):
+
     alphabet = build_alphabet(pattern, text)
+
     dfa = build_kmp_automaton(pattern, alphabet)
     
-    current_state = 0
+    current_state = dfa.starting_state
     count = 0
 
     for i, char in enumerate(text):
         try:
             current_state = dfa.get_next_state(current_state, char)
         except ValueError:
-            current_state = 0
+            current_state = dfa.starting_state
             continue
 
         if dfa.is_accepting_state(current_state):
